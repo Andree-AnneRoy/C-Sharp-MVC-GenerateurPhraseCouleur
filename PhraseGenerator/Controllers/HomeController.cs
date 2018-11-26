78c0759597955f885399e872ec9f22e40825e808
Andree-Anne Roy <andree-anne.roy2009@hotmail.com>
2018-11-25 19:02:46 -0500

    Add project files.

---
+++ PhraseGenerator/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhraseGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult App(string Phrase, string NbFois, string Couleur)
+        {
+            ViewBag.Phrase = Phrase;
+            ViewBag.NbFois = NbFois;
+            ViewBag.Couleur = Couleur;
+
+            return View();
+        }
+    }
+}
